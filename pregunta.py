43b9a7a585f7a3021ccf478e3039240e5baa36f9
carlosmarioca <camchavezag@unal.edu.co>
2023-06-06 14:05:50 -0500

    3

--- pregunta.py
+++ pregunta.py
@@ -13,9 +13,8 @@ from datetime import *
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df = pd.read_csv("solicitudes_credito.csv", sep=";")
     df = df.dropna()
-    df.drop_duplicates(inplace=True)
 
     columns = df.columns.tolist()
     columns_to_format = ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'barrio', 'línea_credito']
@@ -25,17 +24,69 @@ def clean_data():
         df[i] = df[i].str.replace('-', ' ')
         df[i] = df[i].str.replace('_', ' ')
 
-    df["barrio"] = df["barrio"].str.replace(". ",".")
+    df["barrio"] = [i.replace(". ",".") for i in df["barrio"]]
+
+    df["línea_credito"] = [i.replace(". ",".") for i in df["línea_credito"]]
 
     df['comuna_ciudadano'] = df['comuna_ciudadano'].astype('int64')
 
     df["fecha_de_beneficio"] = [datetime.strptime(date, "%d/%m/%Y") if bool(re.search(r"\d{1,2}/\d{2}/\d{4}", date))
                              else datetime.strptime(date, "%Y/%m/%d")
-                             for date in df.fecha_de_beneficio]
+                             for date in df["fecha_de_beneficio"]]
     
-    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: 
-                int(x.replace('$', '').replace(',', '').replace(".0","")))
+    #df['monto_del_credito'] = [int(x.strip("$").replace(',', '').replace(".0","").replace(".00","")) for x in df['monto_del_credito']]
+    df['monto_del_credito'] = df['monto_del_credito'].str.strip('$')
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '')
+    df['monto_del_credito'] = df['monto_del_credito'].astype('float64')
 
+    ind=df.columns.values.tolist()
+    ind.remove('Unnamed: 0')
+    df.drop_duplicates(subset=ind, inplace=True)
+
+    return df.value_counts()
+    
+print(clean_data())
+
+"""
+Limpieza de datos usando Pandas
+-----------------------------------------------------------------------------------------
+
+Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
+correctamente. Tenga en cuenta datos faltantes y duplicados.
+
+"""
+"""
+import pandas as pd
+
+def clean_data():
+
+    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = df.dropna()
+
+    columns = df.columns.tolist()
+    columns_to_format = ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'barrio', 'línea_credito']
+
+    for i in columns_to_format:
+        df[i] = df[i].str.lower()
+        df[i] = df[i].str.replace('-', ' ')
+        df[i] = df[i].str.replace('_', ' ')
+
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype('int64')
+
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],
+                            infer_datetime_format=True,
+                            errors='ignore',
+                            dayfirst=True)
+    
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].dt.strftime("%Y/%m/%d")
+    df['monto_del_credito'] = df['monto_del_credito'].str.strip('$')
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '')
+    df['monto_del_credito'] = df['monto_del_credito'].astype('float64')
+
+    ind=df.columns.values.tolist()
+    ind.remove('Unnamed: 0')
+    df.drop_duplicates(subset=ind, inplace=True)
     return df
 
 print(clean_data())
+"""
